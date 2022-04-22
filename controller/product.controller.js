2ece2bebd6415eb9666e00efb66d7484469aeaee
faizan-lala <faizankhaan6267448042@gmail.com>
2022-04-22 10:47:26 +0530

    ObjectId

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,4 +1,6 @@
+
 const { request, response } = require('express');
+const { default: mongoose } = require('mongoose');
 const productModel=require('../model/product.model');
 
 exports.Add=(request,response)=>{
@@ -44,7 +46,7 @@ exports.viewProduct=(request,response)=>{
 }
 
 exports.showProduct=(request,response)=>{
-    productModel.find({catId:request.body.catId}).then(result=>{
+    productModel.find({catId:mongoose.Types.ObjectId(request.body.catId)}).then(result=>{
         return response.status(200).json(result);
     }).catch(err=>{
         return response.status(500).json({message:'Oops ! Something went wrong'});
